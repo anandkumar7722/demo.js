4f40df708bd514dd10da7452f8c34ae9e274f77b
Anandkumar <ffanand429@gmail.com>
2025-01-09 21:49:53 +0530

    dem

---
+++ demo.js
@@ -0,0 +1,237 @@
+var price=55000
+var product = "ceh"
+var tax=18
+console.log(product)
+var total=price+tax
+console.log(total)
+//next
+var fruitname="apple"
+var count=18
+var price =20
+var total=count*price
+console.log(fruitname)
+console.log("the total price:"+total)
+//operator
+var inc=11
+inc=inc++
+console.log(inc)
+inc=++inc
+console.log(inc)
+var dec=11
+dec=--dec
+console.log(dec)
+dec=dec--
+console.log(dec)
+//data type
+console.log(typeof(11))
+console.log(typeof(dec))
+console.log(typeof(fruitname))
+console.log(typeof(true))
+//function
+function abc(){
+    console.log("working")
+}
+
+function ceh(){
+    console.log("you hacker")
+}
+
+function leet(){
+    console.log("you promblem solve")
+}
+
+function leetdev(){
+    console.log("you promblem solve+dev")
+}
+
+function leetdevhacker(){
+    console.log("you promblem solve+dev+hacker")
+}
+leetdevhacker()
+leetdev()
+leet()
+abc()
+ceh()
+abc()
+function sum (a,b){
+    var tt=a+b
+    console.log("the add:"+tt)
+    var tt=a-b
+    console.log("the sub:"+tt)
+    var tt=a*b
+    console.log("the multi:"+tt)
+    var tt=a/b
+    console.log("the div:"+tt)
+
+}
+var i=0
+var j=0
+/* fun part 
+while(1){
+    console.log("the count of loop"+i)
+    i++
+    j++
+    sum(i,j)
+}
+    */
+function area(l,b){
+    console.log(l*b)
+
+}
+area(10,10000)
+function larea(){
+    //console.log("looood")
+    return "ok"
+
+}
+ result=larea()
+ console.log(result)
+ 
+ /*var state=200
+ var req=200
+ var res=200
+ var port=true
+ var ftp=true
+ */
+
+ /*function check(state, req, res, ftp, port) {
+    if ((state == 200 && req == 200) || port == true || ftp != false) {
+        console.log("the server is working");
+    } else {
+        console.log("the server is not working");
+    }
+}
+
+check(404, 404, true, true, false);
+
+
+ */
+var leave=1
+if (leave==1){
+    console.log("joy")
+}
+else{
+    console.log("!joy")
+}
+console.log(false&& true && true)
+console.log(true && true && true)
+console.log(true || true ||true)
+
+console.log(!true)
+
+//for(var i=0;i<=10;i++){
+//var cheeck=22
+/*if(i%2==0){
+    console.log("even"+i)
+}else{
+    console.log("odd"+i)
+}*/
+
+/*for(i=10;i>=0;i--){
+    console.log(i)
+    
+}*/
+const weekday=7
+const yearday=365
+const timeday=24
+
+
+function sumtwo(a,b){
+    return(a+b)
+}
+a=sumtwo(7,3)
+console.log(a)
+
+function velocity(d,t){
+    var vel=d/t
+    //console.log(vel)
+}
+velocity(986,55)
+ function acce(vo,a,t){
+    var vo
+    v=vo+a*t
+    console.log(v)
+ }
+acce(33,465,45)
+var rain=true
+if(rain==true){
+    console.log("rain")
+}
+else{
+    console.log("!rain")
+}
+var hw="done"
+if(hw=="done"){
+    console.log("good")
+}else{
+    console.log("bad")
+}
+console.log(false && true)
+console.log(false || true)
+console.log(!true)
+
+var state=200
+ var req=200
+ var res=200
+ var port=true
+ var ftp=true
+
+function check(state, req, port, ftp){
+    if ((state == 200 && req == 200) && port == true && ftp ==true) {
+    console.log("the server is working");
+    } else {
+    console.log("the server is not working");
+    }
+ }
+ check(200,200,true,false);
+ check(200,200,true,true);
+function voew(a){
+    if(a=="a"||a=="e"||a=="i"||a=="o"||a=="u"){
+        console.log("vowel")
+    }
+    else{
+    console.log("!vowel")
+    }
+}
+voew("e")
+
+function scorech(score){
+    if(score<50){
+        console.log("bad")
+    }
+    else if(score<=50){
+        console.log("need to improve")
+    }
+    else if(score>80){
+        console.log(" very good")
+    }
+    else if(score>60){
+        console.log("good")
+    }
+    else if(score>50){
+        console.log(" good")
+    }
+    else{
+        console.log("no match")
+    }
+
+}
+scorech(81)
+var i=0,j=0
+//for (i=0;i<=30;i++){
+   // console.log("value i:"+i)
+   // for(j=0;j<=30;j++){
+       // console.log("value :j"+j)
+        
+        
+   // }
+    
+//}
+function table(tables){
+    for(i=0;i<=16;i++){
+        var tt=i*tables
+        console.log(tables+"x"+i+":"+tt)
+    }
+}
+table(4)
+//dom
